4c1dc5f01c95bcc9f927940cd2c5f14a3d33c27a
Mauro <mcvio78@gmail.com>
2020-04-15 21:41:00 +0200

    create BurgerBuilder smart component with Auxiliary hoc inside, use BurgerBuilder component inside Layout as a children element, create Layout.module.css and apply it to main in Layout.js,

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 
 const layout = props => (
 	<Auxiliary>
 		<div>Toolbar, SideDrawer, BackDrop</div>
-		<main>
+		<main className={classes.Content}>
 			{props.children}
 		</main>
 	</Auxiliary>
