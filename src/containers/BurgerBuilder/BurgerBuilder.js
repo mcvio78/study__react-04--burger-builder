fa0f84626c343339bd1208cf7f96be89610271af
Mauro <mcvio78@gmail.com>
2020-04-30 20:47:59 +0200

    Install axios as dependency, create axios-orders.js (instance) and import in BurgerBuilder.js, on purchaseContinueHandler method, create a new Object(order), this Object contains ingredients, price and customer data(hard coded), make a post request with axios sending to firebase realtime database, send order object under orders path.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -35,6 +36,7 @@ class BurgerBuilder extends Component {
 		}).reduce((sum, el) => {
 			return sum + el
 		}, 0);
+
 		this.setState(
 			{ purchasable: sum > 0 }
 		)
@@ -82,15 +84,35 @@ class BurgerBuilder extends Component {
 	};
 
 	purchaseCancelHandler = () => {
-		this.setState( { purchasing: false })
+		this.setState({ purchasing: false })
 	};
 
 	purchaseContinueHandler = () => {
-		alert('You continue!')
+		// alert('You continue!')
+		const order = {
+			ingredients: this.state.ingredients,
+			price: this.state.totalPrice,
+			customer: {
+				name: 'Mauro Vio',
+				address: {
+					street: 'Teststreet 4',
+					zipCode: 763828,
+					country: 'Italy'
+				},
+				email: 'testToTest@test.com'
+			},
+			delivery: 'fastest'
+		};
+		axios.post('/orders.json', order)
+		.then(response => {
+			console.log(response)
+		})
+		.catch(error => {
+			console.log(error)
+		})
 	};
 
 	render() {
-
 		const disableInfo = {
 			...this.state.ingredients
 		};
