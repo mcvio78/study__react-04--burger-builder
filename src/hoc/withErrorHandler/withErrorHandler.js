9881e9e98c7a785fab471fa2bd3b91ff0894181e
Mauro <mcvio78@gmail.com>
2020-05-02 21:45:12 +0200

    BurgerBuilder.js componentWillUnmount eject interceptors, check the values of interceptors removing BurgerBuilder from App.js.

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,16 +10,21 @@ const withErrorHandler = (WrappedComponent, axios) => {
 			error: null
 		};
 
-		UNSAFE_componentWillMount() {
-
-			axios.interceptors.request.use(req => {
+		componentWillMount() {
+			this.reqInterceptor = axios.interceptors.request.use(req => {
 				this.setState({ error: null });
 				return req
 			});
 
-			axios.interceptors.response.use(res => res, error => {
+			this.resInterceptor = axios.interceptors.response.use(res => res, error => {
 				this.setState({ error: error })
 			})
+		}
+
+		componentWillUnmount() {
+			axios.interceptors.request.eject(this.reqInterceptor);
+			axios.interceptors.response.eject(this.resInterceptor);
+			console.log('willUnmount: ', this.reqInterceptor, this.resInterceptor);
 		}
 
 		errorConfirmedHandler = () => {
