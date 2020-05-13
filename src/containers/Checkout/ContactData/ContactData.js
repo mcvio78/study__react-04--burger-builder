076402a485d4f5914fb814f8e3301a9ce590b241
Mauro <mcvio78@gmail.com>
2020-05-13 18:35:44 +0200

    On Input.js create inputClasses array and put classes.InputElement in, use this array on every case in switch statement as className and .join(' '), create .Invalid class in Input.module.css, if props.not valid(state valid) && props.shouldValidate(state elementConfig) from ContactData.js push .Invalid class to inputClasses.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -116,15 +116,15 @@ class ContactData extends Component {
 	checkValidity(value, rules) {
 		let isValid = true;
 
-		if (rules.required) {
+		if (rules && rules.required) {
 			isValid = value.trim() !== '' && isValid;
 		}
 
-		if (rules.minLength) {
+		if (rules && rules.minLength) {
 			isValid = value.length >= rules.minLength && isValid;
 		}
 
-		if (rules.maxLength) {
+		if (rules && rules.maxLength) {
 			isValid = value.length <= rules.maxLength && isValid;
 		}
 
@@ -172,6 +172,8 @@ class ContactData extends Component {
 							elementConfig={formElement.config.elementConfig}
 							value={formElement.config.value}
 							key={formElement.id}
+							notValid={!formElement.config.valid}
+							shouldValidate={formElement.config.validation}
 							changed={(event) => this.inputChangeHandler(event, formElement.id)}/>
 					)
 				)}
