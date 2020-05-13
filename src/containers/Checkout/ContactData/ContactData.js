c00d1d5cd1e39238bc042d15cbcddf14011fff36
Mauro <mcvio78@gmail.com>
2020-05-13 21:04:50 +0200

    Improve handling method if there is no rules(validation in state) for an input inside the form.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,6 +85,7 @@ class ContactData extends Component {
 					]
 				},
 				value: '',
+				validation: {},
 				valid: true
 			}
 		},
@@ -123,15 +124,19 @@ class ContactData extends Component {
 	checkValidity(value, rules) {
 		let isValid = true;
 
-		if (rules && rules.required) {
+		if(!rules){
+			return true
+		}
+
+		if (rules.required) {
 			isValid = value.trim() !== '' && isValid;
 		}
 
-		if (rules && rules.minLength) {
+		if (rules.minLength) {
 			isValid = value.length >= rules.minLength && isValid;
 		}
 
-		if (rules && rules.maxLength) {
+		if (rules.maxLength) {
 			isValid = value.length <= rules.maxLength && isValid;
 		}
 
