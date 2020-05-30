fadccd433692222cdde2b3301fe679f80a1f612f
Mauro <mcvio78@gmail.com>
2020-05-30 16:57:48 +0200

    Create utility.js function which receives an old Object and updated properties, return an Object with distributed properties from the old Object and updates other properties with the updated one, [reducers/burgerBuilder.js][reducers/order.js] lean code inside switch statement.

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   ingredients: null,
@@ -12,48 +13,53 @@ const INGREDIENT_PRICES = {
   meat: 1.3,
   bacon: 0.7
 };
-
+/* eslint-disable */
 const burgerBuilderReducer = (state = initialState, action) => {
   switch (action.type) {
 
-    case actionTypes.ADD_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-        },
+    case actionTypes.ADD_INGREDIENT: {
+      const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 };
+      const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+      const stateUpdatedProperties = {
+        ingredients: updatedIngredients,
         totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
       };
+      return updateObject(state, stateUpdatedProperties);
+    }
 
-    case actionTypes.REMOVE_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-        },
+    case actionTypes.REMOVE_INGREDIENT: {
+      const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 };
+      const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+      const stateUpdatedProperties = {
+        ingredients: updatedIngredients,
         totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
       };
+      return updateObject(state, stateUpdatedProperties);
+    }
+    /* eslint-enable */
 
-    case actionTypes.SET_INGREDIENTS:
-      return {
-        ...state,
-        ingredients: {
-          salad: action.ingredients.salad,
-          bacon: action.ingredients.bacon,
-          cheese: action.ingredients.cheese,
-          meat: action.ingredients.meat
-        },
+    case actionTypes.SET_INGREDIENTS: {
+      const initIngredients = {
+        salad: action.ingredients.salad,
+        bacon: action.ingredients.bacon,
+        cheese: action.ingredients.cheese,
+        meat: action.ingredients.meat
+      };
+      const updatedIngredients = updateObject(state.ingredients, initIngredients); // eslint-disable-line
+      const stateUpdatedProperties = {
+        ingredients: updatedIngredients,
         error: false,
         totalPrice: 4
       };
+      return updateObject(state, stateUpdatedProperties);
+    }
 
-    case actionTypes.FETCH_INGREDIENTS_FAILED:
-      return {
-        ...state,
+    case actionTypes.FETCH_INGREDIENTS_FAILED: {
+      const stateUpdatedProperties = {
         error: true
       };
+      return updateObject(state, stateUpdatedProperties);
+    }
 
     default:
       return state;
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   orders: [],
@@ -11,58 +12,49 @@ const orderReducer = (state = initialState, action) => {
   switch (action.type) {
 
     case actionTypes.PURCHASE_INIT:
-      return {
-        ...state,
-        purchased: false
-      };
+      return updateObject(state, { purchased: false });
 
     case actionTypes.PURCHASE_BURGER_START:
-      return {
-        ...state,
-        loading: true
-      };
+      return updateObject(state, { loading: true });
 
     case actionTypes.PURCHASE_BURGER_SUCCESS: {
-      const newOrder = {
-        ...action.orderData,
-        id: action.orderId
-      };
+      const newOrder = updateObject(action.orderData, { id: action.orderId });
 
-      return {
-        ...state,
+      const stateUpdatedProperties = {
         loading: false,
         orders: state.orders.concat(newOrder),
         purchased: true
       };
+      return updateObject(state, stateUpdatedProperties);
     }
 
-    case actionTypes.PURCHASE_BURGER_FAIL:
-      return {
-        ...state,
+    case actionTypes.PURCHASE_BURGER_FAIL: {
+      const stateUpdatedProperties = {
         loading: false,
         error: true
       };
+      return updateObject(state, stateUpdatedProperties);
+    }
 
-    case actionTypes.FETCH_ORDER_START:
-      return {
-        ...state,
-        loading: true
-      };
+    case actionTypes.FETCH_ORDER_START: {
+      return updateObject(state, { loading: true });
+    }
 
-    case actionTypes.FETCH_ORDER_SUCCESS:
-      return {
-        ...state,
+    case actionTypes.FETCH_ORDER_SUCCESS:{
+      const stateUpdatedProperties = {
         orders: action.orders,
         loading: false
       };
+      return updateObject(state, stateUpdatedProperties);
+    }
 
-    case actionTypes.FETCH_ORDER_FAIL:
-      return {
-        ...state,
+    case actionTypes.FETCH_ORDER_FAIL:{
+      const stateUpdatedProperties = {
         error: true,
         loading: false
       };
-
+      return updateObject(state, stateUpdatedProperties);
+    }
 
     default:
       return state;
