6b2931194692b0bf102fef73f7edb568dfbc8d50
Mauro <mcvio78@gmail.com>
2020-06-02 21:05:00 +0200

    [Layout.js] connect to the state.auth.token and check if is not null, pass it to SideDrawer.js and Toolbar.js as props, [SideDrawer.js] & [Toolbar.js] receive token value as a prop and pass it to NavigationItems.js, [NavigationItems.js] if token is null show authentication NavigationItem NavLink to /auth, if token is not null show logout NavigationItem NavLint to /logout.

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Auxiliary from '../Auxiliary/Auxiliary';
 import classes from './Layout.module.css';
@@ -23,10 +24,13 @@ class Layout extends Component {
   render() {
     return (
       <Auxiliary>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <Toolbar
+          drawerToggleClicked={this.sideDrawerToggleHandler}
+          isAuth={this.props.isAuthenticated}/>
         <SideDrawer
           closed={this.sideDrawerCloseHandler}
-          open={this.state.showSideDrawer}/>
+          open={this.state.showSideDrawer}
+          isAuth={this.props.isAuthenticated}/>
         <main className={classes.Content}>
           {this.props.children}
         </main>
@@ -35,4 +39,10 @@ class Layout extends Component {
   }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
+export default connect(mapStateToProps)(Layout);
