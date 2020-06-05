540b21ff2beb8c5db8cff525234df872cbb88465
Mauro <mcvio78@gmail.com>
2020-06-05 21:29:06 +0200

    Use environment variable which checks if is development mode and set REDUX_DEVTOOLS_EXTENSION_COMPOSE.

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const logger = store => {
 };
 
 // eslint-disable-next-line
-const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) : null || compose;
 
 const rootReducer = combineReducers({
   brgBld: burgerBuilderReducer,
